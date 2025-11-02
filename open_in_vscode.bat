245bfe4b5420038b51f318f187c75003c3ea06af
LostLinker Developer <developer@example.com>
2025-11-02 21:22:19 +0530

    Deleted unwanted files from frontend

--- open_in_vscode.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Opening project in VS Code...
-"%LOCALAPPDATA%\Programs\Microsoft VS Code\Code.exe" "c:\Users\Hp\Documents\suhan\web dev"
-if %errorlevel% neq 0 (
-    echo Failed to open VS Code. Please make sure it's installed correctly.
-    echo You can also try manually opening VS Code and then selecting:
-    echo File ^> Open Folder ^> "c:\Users\Hp\Documents\suhan\web dev"
-    pause
-)
--- open_project.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Opening Project Expo in VS Code...
-cd /d "c:\Users\Hp\Documents\suhan\web dev\project expo"
-code .
-echo If VS Code didn't open, please make sure it's installed and added to your PATH
-pause
